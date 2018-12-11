a1008692da8f5bf5770ad90f82ac50da0a8920d2
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-12-11 23:57:52 +0530

    Redux DevTools

--- src/core/store.js
+++ src/core/store.js
@@ -1,16 +1,17 @@
 /**
  * Created by vaibhav on 16/4/18
  */
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        composeEnhancers(applyMiddleware(thunk))
     );
 }
